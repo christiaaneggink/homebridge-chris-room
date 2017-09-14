c2d037006a204be59ef0dd80582251157f2bc433
christiaaneggink <31407065+christiaaneggink@users.noreply.github.com>
2017-09-14 19:36:50 +0800

    Add files via upload
    Added support for battery monitoring and a homekit battery service

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ Remember to add accessory to config.json. Example:
         "topic_t": "home/livingroom/temperature",
         "topic_h": "home/livingroom/humidity",
         "topic_a": "home/livingroom/airquality",
+        "topic_batt_chg": "home/livingroom/batt_charging",
+        "topic_batt_lvl": "home/livingroom/batt_level",
+        "topic_batt_low": "home/livingroom/batt_low",
         "username": "username",
         "password": "password",
-        "serial": "HMH-54D3X"
+        "serial": "CR001-001"
 	}
 ],
 
@@ -36,6 +39,9 @@ function ChrisRoomAccessory(log, config) {
   this.topic_t = config['topic_t'];
   this.topic_h = config['topic_h'];
   this.topic_a = config['topic_a'];
+  this.topic_batt_chg = config['topic_batt_chg'];
+  this.topic_batt_lvl = config['topic_batt_lvl'];
+  this.topic_batt_low = config['topic_batt_low'];
   this.client_Id = 'mqttjs_' + Math.random().toString(16).substr(2, 8);
   this.options = {
     keepalive: 10,
@@ -60,11 +66,15 @@ function ChrisRoomAccessory(log, config) {
   this.service_t = new Service.TemperatureSensor(this.name);
   this.service_h = new Service.HumiditySensor(this.name);
   this.service_a = new Service.AirQualitySensor(this.name);
+  this.service_b = new Service.BatteryService(this.name);
   
-  this.client  = mqtt.connect(this.url, this.options);  
+  this.client = mqtt.connect(this.url, this.options);  
   this.client.subscribe(this.topic_t);
   this.client.subscribe(this.topic_h);
   this.client.subscribe(this.topic_a);
+  this.client.subscribe(this.topic_batt_chg);
+  this.client.subscribe(this.topic_batt_lvl);
+  this.client.subscribe(this.topic_batt_low);
 
   var that = this;
   this.client.on('message', function (topic, message) {
@@ -95,7 +105,31 @@ function ChrisRoomAccessory(log, config) {
       that.service_a
         .setCharacteristic(Characteristic.AirQuality, that.airquality);    
     }
-    
+
+    // Battery Charging
+    if (topic.indexOf('/batt_charging') !== -1) {
+      that.batt_chg = parseFloat(data);
+      that.log(that.name, "- MQTT (battery charging): ", that.batt_chg);
+      that.service_b
+        .setCharacteristic(Characteristic.ChargingState, that.batt_chg);    
+    }
+
+    // Battery Level
+    if (topic.indexOf('/batt_level') !== -1) {
+      that.batt_lvl = parseFloat(data);
+      that.log(that.name, "- MQTT (battery level): ", that.batt_lvl);
+      that.service_b
+        .setCharacteristic(Characteristic.BatteryLevel, that.batt_lvl);    
+    }
+
+    // Battery Status Low
+    if (topic.indexOf('/batt_low') !== -1) {
+      that.batt_low = parseFloat(data);
+      that.log(that.name, "- MQTT (battery status low): ", that.batt_low);
+      that.service_b
+        .setCharacteristic(Characteristic.StatusLowBattery, that.batt_low);    
+    }
+        
   });
 
   this.service_t
@@ -109,6 +143,18 @@ function ChrisRoomAccessory(log, config) {
   this.service_a
     .getCharacteristic(Characteristic.AirQuality)
     .on('get', this.getState_a.bind(this));
+   
+  this.service_b
+    .getCharacteristic(Characteristic.ChargingState)
+    .on('get', this.getState_batt_chg.bind(this));
+
+  this.service_b
+    .getCharacteristic(Characteristic.BatteryLevel)
+    .on('get', this.getState_batt_lvl.bind(this));
+
+  this.service_b
+    .getCharacteristic(Characteristic.StatusLowBattery)
+    .on('get', this.getState_batt_low.bind(this));
 }
 
 ChrisRoomAccessory.prototype.getState_t = function(callback) {
@@ -126,6 +172,21 @@ ChrisRoomAccessory.prototype.getState_a = function(callback) {
   callback(null, this.airquality);
 }
 
+ChrisRoomAccessory.prototype.getState_batt_chg = function(callback) {
+  this.log(this.name, "- MQTT (battery charging) : ", this.batt_chg);
+  callback(null, this.batt_chg);
+}
+
+ChrisRoomAccessory.prototype.getState_batt_lvl = function(callback) {
+  this.log(this.name, "- MQTT (battery level) : ", this.batt_lvl);
+  callback(null, this.batt_lvl);
+}
+
+ChrisRoomAccessory.prototype.getState_batt_low = function(callback) {
+  this.log(this.name, "- MQTT (battery low) : ", this.batt_low);
+  callback(null, this.batt_low);
+}
+
 ChrisRoomAccessory.prototype.getServices = function() {
   var informationService = new Service.AccessoryInformation();
   informationService
@@ -133,5 +194,5 @@ ChrisRoomAccessory.prototype.getServices = function() {
     .setCharacteristic(Characteristic.Model, "Chris Room")
     .setCharacteristic(Characteristic.SerialNumber, this.options["serialnumber"]);
 
-  return [informationService, this.service_t, this.service_h, this.service_a];
+  return [informationService, this.service_t, this.service_h, this.service_a, this.service_b];
 }
